abc0209e03ef1a9dd3712b0f9ddb72f654db796d
Valentin Samir <valentin.samir@crans.org>
2012-06-14 09:04:39 +0200

    On rajoute la date lorsque l'on cite un message précédent

--- webnews/compose_message.php
+++ webnews/compose_message.php
@@ -36,11 +36,10 @@
 						$message .= decode_message_content($part);
 					}
 				}
-				
 				$message = wordwrap($message, 75, "\r\n", true);
                                 $message = preg_replace("/\n-- \r\n(.*)/s","\n",$message);
-				$message = preg_replace("/(.*\r\n)/", "&gt; $1", htmlescape($message));
-				$message = ($header["from"]["name"])." ".$messages_ini["text"]["wrote"].":\r\n\r\n".($message);
+				$message = preg_replace("/(.*\r\n)/", "&gt;$1", htmlescape($message));
+				$message = date($messages_ini['text']['date_format'], strtotime($header['date'])).($header["from"]["name"])." ".$messages_ini["text"]["wrote"].":\r\n".($message);
 			}
 			// Quit sooner to release resources		
 			$nntp->quit();
